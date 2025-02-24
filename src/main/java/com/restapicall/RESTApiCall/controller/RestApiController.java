a1fd6843daa08a1b20e9e9c3016b17da988f3447
PRINCE RAJ <rajprince031@gmail.com>
2025-02-24 13:36:51 +0530

    [Prince Raj] add . uc2 Using Path param

--- src/main/java/com/restapicall/RESTApiCall/controller/RestApiController.java
+++ src/main/java/com/restapicall/RESTApiCall/controller/RestApiController.java
@@ -1,16 +1,23 @@
 package com.restapicall.RESTApiCall.controller;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class RestApiController {
 
     //uc1 : Base GET Request
-    @GetMapping
+    @GetMapping("/")
     public String hello(){
         return "Hello from BridgeLabz";
     }
+
+    //uc2 : Using Path PathParam
+    @GetMapping("/name")
+    public String callingName(@PathParam("name") String name){
+        return "Hello "+name;
+    }
+
 }
